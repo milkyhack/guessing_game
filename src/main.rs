8e51645c7339e2f8020cb4955a313444a5052e77
maks <65305777+milkyhack@users.noreply.github.com>
2022-04-04 19:31:05 +0500

    new rust

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Угадай цифру");
+loop{
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("Введи число чтоб угадать загаданное");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Ошибка чтения");
+
+let guess : u32 = match guess.trim().parse() {
+    Ok(num) => num,
+    Err(_) => continue,
+};
+
+    println!("Твоё число : {}", guess);
+
+    match guess.cmp(&secret_number){
+        Ordering::Less=> println!("меньше загаданного"),
+        Ordering::Greater=>println!("Больше загаданного"),
+        Ordering::Equal=> {
+            println!("Ты выиграл");
+            break;
+}
+        }
+    }
+}
