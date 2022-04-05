912350ab21c445929d94772e469805c3065495d0
maks <65305777+milkyhack@users.noreply.github.com>
2022-04-05 09:12:59 +0500

    change string 7,there was a mistake.

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::cmp::Ordering;
 
 fn main() {
     println!("Угадай цифру");
-loop{
     let secret_number = rand::thread_rng().gen_range(1..101);
+loop{
 
     println!("Введи число чтоб угадать загаданное");
 
